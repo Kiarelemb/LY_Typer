4d2439f5c77d801a4375f02f26f9aa75954e7497
Kiarelemb <kiarelemb@outlook.com>
2024-04-14 15:18:54 +0800

    Update

--- src/ly/qr/kiarelemb/data/Keys.java
+++ src/ly/qr/kiarelemb/data/Keys.java
@@ -367,6 +367,7 @@ public class Keys {
             put(TEXT_TIP_WINDOW_LOCATION, "0");
             put(SEND_TEXT_NEW_WINDOW_TAB_INDEX, "0");
             put(TEXT_SEND_START_PARA, "0");
+            put(TEXT_SEND_START_WORD_NUM, "50");
             put(SEND_MINIMALISM, "false");
             put(SEND_CRYPTOGRAPHIC, "false");
             put(SEND_BACK_CHANGE, "true");
