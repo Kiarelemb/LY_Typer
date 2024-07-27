798858a84f3da44c35c4c499b3c1000a23c158fa
Kiarelemb <Kiarelemb@outlook.com>
2024-07-27 23:44:55 +0800

    update

--- src/ly/qr/kiarelemb/type/KeyTypedRecordWindow.java
+++ src/ly/qr/kiarelemb/type/KeyTypedRecordWindow.java
@@ -3,7 +3,6 @@ package ly.qr.kiarelemb.type;
 import ly.qr.kiarelemb.MainWindow;
 import ly.qr.kiarelemb.component.KeyBoardPanel;
 import ly.qr.kiarelemb.data.KeyTypedRecordData;
-import swing.qr.kiarelemb.combination.QRTabbedContentPanel;
 import swing.qr.kiarelemb.combination.QRTabbedPane;
 import swing.qr.kiarelemb.window.basic.QRDialog;
 
@@ -20,23 +19,28 @@ import java.awt.*;
 public class KeyTypedRecordWindow extends QRDialog {
     public KeyTypedRecordWindow() {
         super(MainWindow.INSTANCE);
-
+        setParentWindowNotFollowMove();
         setTitle("键盘热力图");
         setTitlePlace(SwingConstants.CENTER);
         mainPanel.setLayout(new BorderLayout());
 
-        KeyBoardPanel todayKeyBoard = new KeyBoardPanel(KeyTypedRecordData.KEY_TODAY_RECORD_DATA.keyMap);
-        KeyBoardPanel totalKeyBoard = new KeyBoardPanel(KeyTypedRecordData.KEY_TOTAL_RECORD_DATA.keyMap);
-
-        QRTabbedPane tabbedPane = new QRTabbedPane(BorderLayout.NORTH, FlowLayout.CENTER);
-
-        QRTabbedContentPanel today = new KeyBoardPanel.TabContentPanel(todayKeyBoard, totalKeyBoard);
-        QRTabbedContentPanel total = new KeyBoardPanel.TabContentPanel(totalKeyBoard, todayKeyBoard);
-
-        tabbedPane.addTab("今日跟打记录", today);
-        tabbedPane.addTab("总计跟打记录", total);
+        var tabbedPane = init_tabbedPane();
         tabbedPane.setSelectedTab(!KeyTypedRecordData.KEY_TODAY_RECORD_DATA.keyMap.isEmpty() ? 0 : 1);
         mainPanel.add(tabbedPane, BorderLayout.CENTER);
         setSize(1360, 610);
+    }
+
+    private static QRTabbedPane init_tabbedPane() {
+        var todayKeyBoard = new KeyBoardPanel(KeyTypedRecordData.KEY_TODAY_RECORD_DATA.keyMap);
+        var totalKeyBoard = new KeyBoardPanel(KeyTypedRecordData.KEY_TOTAL_RECORD_DATA.keyMap);
+
+        var tabbedPane = new QRTabbedPane(BorderLayout.NORTH, FlowLayout.CENTER);
+
+        var today = new KeyBoardPanel.TabContentPanel(todayKeyBoard, totalKeyBoard);
+        var total = new KeyBoardPanel.TabContentPanel(totalKeyBoard, todayKeyBoard);
+
+        tabbedPane.addTab("今日跟打记录", today);
+        tabbedPane.addTab("总计跟打记录", total);
+        return tabbedPane;
     }
 }
